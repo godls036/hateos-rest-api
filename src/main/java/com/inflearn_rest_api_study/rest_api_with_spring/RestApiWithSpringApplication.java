41918839045d4ec2378ebdc57a0135a83b47ae87
hicho <hicho@containus.kr>
2025-01-12 18:22:02 +0900

    fist commit

---
+++ src/main/java/com/inflearn_rest_api_study/rest_api_with_spring/RestApiWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn_rest_api_study.rest_api_with_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inflearn_rest_api_study/rest_api_with_spring/RestApiWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn_rest_api_study.rest_api_with_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
